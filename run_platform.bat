f13d1e27fc05b8c142ca78110b5bc1753c01945e
sesozien <sesozien00@gmail.com>
2026-06-30 04:55:03 +0300

    Add files via upload

---
+++ run_platform.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+title 🛸 Bo0'sViDClone Launcher 🛸
+echo =========================================
+echo       ⚡ جاري إطلاق إمبراطورية مـنتــجـك ⚡
+echo =========================================
+echo.
+echo [+] Checking required packages...
+pip install streamlit pillow moviepy yt-dlp requests beautifulsoup4 opencv-python numpy
+echo.
+echo [+] Launching Streamlit Server...
+streamlit run app.py
+pause
